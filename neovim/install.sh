4a356b62acfa88a02f949ff62e59e0996887609a
DanielAtDito <daniel@ditoweb.com>
2016-02-24 07:36:20 -0600

    python3-dev python3-pip

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y neovim
-sudo apt-get install -y python-dev python-pip
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
