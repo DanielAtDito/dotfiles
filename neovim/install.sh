a0c3006b8b360f76c1b594f08e3e2200b68075ea
Daniel Rhoden <daniel@ditoweb.com>
2016-02-24 18:30:44 +0000

    NeoVim Python support with pip2/pip3

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,10 +1,11 @@
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install -y neovim
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y neovim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+sudo pip2 install neovim
+sudo pip3 install neovim
 nvim +PlugInstall +qall
