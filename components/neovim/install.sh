a63af2f3b62bb7958bf7bccc1d94adfc92be2b8b
Daniel Rhoden <daniel@ditoweb.com>
2016-03-17 19:48:29 +0000

    Simplified, package, repo, and ordered install approach

--- components/neovim/install.sh
+++ components/neovim/install.sh
@@ -1,12 +1,7 @@
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-sudo apt-get install -y neovim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo pip2 install neovim
 sudo pip3 install neovim
-nvim +PlugInstall +qall
+nvim "Please-Wait--Plugins-Are-Installing" +PlugInstall +qall
 ~/.vim/plugged/YouCompleteMe/install.py --gocode-completer --tern-completer
--- debian/install.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y build-essential libtool libtool-bin autoconf automake cmake g++ pkg-config unzip libmsgpack-dev libuv-dev libluajit-5.1-dev 
-
